e33e6a71dd97ebc80fedf185db29328b1d941c1f
Ryan Fleming <rflemin3@terpmail.umd.edu>
2024-10-21 01:48:59 -0400

    remove ubuntu line

--- src/honey.sh
+++ src/honey.sh
@@ -11,7 +11,6 @@ zip_path=/home/student/honeypot-group-1a/honey/hbooth.zip
 
 sudo unzip "$zip_path" -d /var/lib/lxc/"$cont_name"/rootfs/home
 sudo lxc-attach -n "$cont_name" -- sudo useradd -d /home/hbooth hbooth
-sudo lxc-attach -n "$cont_name" -- sudo cp -r /home/ubuntu/* /home/hbooth
 
 exit 0
 
